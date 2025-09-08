2efc1c41226dd7b8a6117fa2e6a0129a26ece284
Shashi Nagarajan <shashiniyer@gmail.com>
2025-09-08 14:20:43 +0200

    init + some reorg

--- last_job.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/sh                                                                                 
-LAST_JOBID=$(sacct -X -o jobid | tail -n 1)                                               
-srun --pty --jobid $LAST_JOBID /bin/bash
